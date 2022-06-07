1178c661cc797307ebd4b7c148a15749cc964c67
gmluqa <gmluqa@protonmail.com>
2022-06-07 19:43:44 +0200

    Added namedAccounts, deployer and user

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,10 +27,7 @@ module.exports = {
     networks: {
         ropsten: {
             url: process.env.ROPSTEN_URL || "",
-            accounts:
-                process.env.PRIVATE_KEY !== undefined
-                    ? [process.env.PRIVATE_KEY]
-                    : [],
+            accounts: [],
         },
     },
     gasReporter: {
@@ -39,5 +36,13 @@ module.exports = {
     },
     etherscan: {
         apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
     },
 }
