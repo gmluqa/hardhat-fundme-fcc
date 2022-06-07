676de0ea0ac09d649a0a9aa697c2277fc8743d5b
gmluqa <gmluqa@protonmail.com>
2022-06-08 01:31:32 +0200

    configured rinkeby, gasreporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,19 +6,6 @@ require("hardhat-gas-reporter")
 require("solidity-coverage")
 require("hardhat-deploy")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners()
-
-    for (const account of accounts) {
-        console.log(account.address)
-    }
-})
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -27,14 +14,19 @@ module.exports = {
         compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
     },
     networks: {
-        ropsten: {
-            url: process.env.ROPSTEN_URL || "",
-            accounts: [],
+        rinkeby: {
+            url: process.env.RINKEBY_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY], // uses the private key
+            chainId: 4,
+            blockConfirmations: 6, //Wait 6 blocks, refrenced in 01-deploy
         },
     },
     gasReporter: {
-        enabled: process.env.REPORT_GAS !== undefined,
+        enabled: false,
+        outputFile: "gas-reporter.txt",
+        noColors: true,
         currency: "USD",
+        coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
     etherscan: {
         apiKey: process.env.ETHERSCAN_API_KEY,
