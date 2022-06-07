b7f0a2f8dc735ea258f4b43e649884f86bd93234
gmluqa <gmluqa@protonmail.com>
2022-06-07 23:59:04 +0200

    Made it so that mockv3 would auto get from dev chain, testnet or mainnets

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -3,6 +3,8 @@
 // }
 
 const { deployments } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
 
 // module.exports.default = deployFunc
 
@@ -11,6 +13,24 @@ module.exports = async ({ getNamedAccounts, deployents }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts() //grabs deployer from namedAccounts in hh.cnfg.js
     const chainId = network.config.chainId
+    // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
 
     //when going for localhost or hardhat, use mock
+
+    //const fundMe represents deploying of Contract(.sol)
+    const fundMe = await deploy("FundMe", {
+        from: deployer, // grabs from namedAccounts in hh.cnfg.js
+        args: [ethUsdPriceFeedAddress], // put price feed address
+        log: true,
+    })
+    console.log("------------------")
 }
+module.exports.tags = ["all", "FundMe"]
