1597720f0afbd42e504f7e7425d16cf6a3f98f13
gmluqa <gmluqa@protonmail.com>
2022-06-08 09:58:23 +0200

    added fund.js as quick script to be execd

---
+++ scripts/fund.js
@@ -0,0 +1,23 @@
+//`yarn hardhat node`
+//open new terminal
+//`yarn hardhat run scripts/fund.js --network localhost`
+
+const { getNamedAccounts, ethers } = require("hardhat")
+
+async function main() {
+    const { deployer } = await getNamedAccounts()
+    const fundMe = await ethers.getContract("FundMe", deployer)
+    console.log("Funding contract...")
+    const transactionResponse = await fundMe.fund({
+        value: ethers.utils.parseEther("0.1"),
+    })
+    await transactionResponse.wait(1)
+    console.log("Funded!")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
