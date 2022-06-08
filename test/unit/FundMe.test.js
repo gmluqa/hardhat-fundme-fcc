46e6da55de1bfe8a8123b60d1356b7f82f1e47a1
gmluqa <gmluqa@protonmail.com>
2022-06-08 02:40:03 +0200

    finished and completed first test

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,5 +1,28 @@
 // unit tests are for specific parts of code, units
 
+//Imports the deployments module
+const { deployments, ethers, getNamedAccounts } = require("hardhat")
+const { assert } = require("chai")
+
 describe("FundMe", async function () {
-    beforeEach()
+    let fundMe
+    let deployer
+    let mockV3Aggregator
+    beforeEach(async function () {
+        // const accounts = await ethers.getSigners() // returns accounts signing
+        deployer = (await getNamedAccounts()).deployer // gets deployer address
+        await deployments.fixture(["all"]) //runs through all scripts with all tag
+        fundMe = await ethers.getContract("FundMe", deployer)
+        mockV3Aggregator = await ethers.getContract(
+            "MockV3Aggregator",
+            deployer
+        )
+    })
+
+    describe("constructor", async function () {
+        it("Sets the aggregator addresses correctly", async function () {
+            const response = await fundMe.priceFeed()
+            assert.equal(response, mockV3Aggregator.address)
+        })
+    })
 })
