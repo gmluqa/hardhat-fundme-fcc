0f398e2950573df55e8ea703da0b467854a13b17
gmluqa <gmluqa@protonmail.com>
2022-06-08 06:54:46 +0200

    Added correct naming conventions to vars

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -27,15 +27,18 @@ error FundMe__NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    mapping(address => uint256) public addressToAmountFunded;
-    address[] public funders;
+    //storage vars declared with s_var
+    mapping(address => uint256) public s_addressToAmountFunded;
+    address[] public s_funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
+    //immutable vars declared with i_var
     address public immutable i_owner;
+    // constant caps_locked
     uint256 public constant MINIMUM_USD = 50 * 10**18;
 
     //Aggregatorv3interface with 0x address from constructor, interface acts as ABI and address is target
-    AggregatorV3Interface public priceFeed;
+    AggregatorV3Interface public s_priceFeed;
 
     modifier onlyOwner() {
         // require(msg.sender == owner);
@@ -43,32 +46,32 @@ contract FundMe {
         _;
     }
 
-    //Constructs pricefeed address depending on chain (configged in deploy)
-    constructor(address priceFeedAddress) {
+    //Constructs s_priceFeed address depending on chain (configged in deploy)
+    constructor(address s_priceFeedAddress) {
         i_owner = msg.sender;
-        priceFeed = AggregatorV3Interface(priceFeedAddress);
+        s_priceFeed = AggregatorV3Interface(s_priceFeedAddress);
     }
 
     function fund() public payable {
         require(
-            msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
             "You need to spend more ETH!"
         );
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
     }
 
     function withdraw() public payable onlyOwner {
         for (
             uint256 funderIndex = 0;
-            funderIndex < funders.length;
+            funderIndex < s_funders.length;
             funderIndex++
         ) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
+        s_funders = new address[](0);
         // // transfer
         // payable(msg.sender).transfer(address(this).balance);
         // // send
